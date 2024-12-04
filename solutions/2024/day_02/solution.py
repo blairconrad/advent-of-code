79d32728365264e8263cb9a2cf855e3cff7e5a44
Blair Conrad <blair@blairconrad.com>
2024-12-03 20:44:14 -0500

    Solve 2024 Day  2 Part 2

--- solutions/2024/day_02/solution.py
+++ solutions/2024/day_02/solution.py
@@ -3,15 +3,15 @@
 # puzzle prompt: https://adventofcode.com/2024/day/2
 
 
-from collections.abc import Iterable
-from itertools import pairwise
+from collections.abc import Callable, Iterable
+from itertools import count, islice, pairwise
 
-from pipe import select
+from pipe import select, take
 
 from solutions.base import StrSplitSolution, answer
 
 
-def count(iterable: Iterable[int]) -> int:
+def how_many(iterable: Iterable[int]) -> int:
     return sum(1 for i in iterable)
 
 
@@ -27,10 +27,26 @@ def is_safe_descending(first: int, second: int) -> bool:
     return is_safe_ascending(second, first)
 
 
-def is_safe(level: tuple[int, int, int, int, int]) -> bool:
+def nth(n: int) -> int:
+    return lambda iter: next(islice(iter, n, None), None)
+
+
+def is_safe(level: tuple[int, ...]) -> bool:
     return all(is_safe_ascending(a, b) for a, b in pairwise(level)) or all(
         is_safe_descending(a, b) for a, b in pairwise(level)
     )
+
+
+def drop_from(level: tuple[int, ...]) -> Callable[[int], tuple[int, ...]]:
+    def drop(i: int) -> tuple[int, ...]:
+        return level[:i] + level[i + 1 :]
+
+    return drop
+
+
+def is_safe_dampened(level: tuple[int, ...]) -> bool:
+    n = len(level)
+    return any(count(0) | take(n) | select(drop_from(level)) | select(is_safe))
 
 
 class Solution(StrSplitSolution):
@@ -39,12 +55,8 @@ class Solution(StrSplitSolution):
 
     @answer(639)
     def part_1(self) -> int:
-        return count(filter(is_safe, self.input | select(parse_ints)))
+        return how_many(filter(is_safe, self.input | select(parse_ints)))
 
-    # @answer(1234)
+    @answer(674)
     def part_2(self) -> int:
-        pass
-
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
+        return how_many(filter(is_safe_dampened, self.input | select(parse_ints)))
