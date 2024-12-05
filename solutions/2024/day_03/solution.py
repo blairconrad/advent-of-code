31851125e18ff4f99a74c8ed3fd1f26ad59ec1cc
Blair Conrad <blair@blairconrad.com>
2024-12-05 06:23:31 -0500

    Solve 2024 Day  3 Part 2

--- solutions/2024/day_03/solution.py
+++ solutions/2024/day_03/solution.py
@@ -4,6 +4,8 @@
 
 import re
 from collections.abc import Iterable
+from itertools import chain
+from operator import mul
 
 from pipe import Pipe, select
 
@@ -14,17 +16,23 @@ class Solution(TextSolution):
     _year = 2024
     _day = 3
 
+    def find_active_areas(self, program: str) -> Iterable[str]:
+        return re.split(r"don't\(\).*?(?:do\(\)|$)", program, flags=re.DOTALL)
+
     def find_muls(self, program: str) -> Iterable[tuple[int, int]]:
         return re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", program)
 
     @answer(179834255)
     def part_1(self) -> int:
-        return sum(self.find_muls(self.input) | select(lambda x: int(x[0]) * int(x[1])))
+        return self.find_muls(self.input) | select(lambda x: x | select(int)) | select(lambda x: mul(*x)) | Pipe(sum)
 
-    # @answer(1234)
+    @answer(80570939)
     def part_2(self) -> int:
-        pass
-
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
+        return (
+            self.find_active_areas(self.input)
+            | select(self.find_muls)
+            | Pipe(chain.from_iterable)
+            | select(lambda x: x | select(int))
+            | select(lambda x: mul(*x))
+            | Pipe(sum)
+        )
