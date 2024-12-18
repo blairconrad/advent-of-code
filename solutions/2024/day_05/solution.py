ca4a2b3de5900fc5341383ad5c34ba7e6f7d3820
Blair Conrad <blair@blairconrad.com>
2024-12-18 06:38:31 -0500

    Solve 2024 Day  5 Part 2

--- solutions/2024/day_05/solution.py
+++ solutions/2024/day_05/solution.py
@@ -3,7 +3,7 @@
 # puzzle prompt: https://adventofcode.com/2024/day/5
 
 from collections.abc import Callable, Iterable
-from functools import partial
+from functools import cmp_to_key, partial
 
 from pipe import select, where
 
@@ -25,20 +25,20 @@ def int_tuple(separator: str) -> Callable[[str], tuple[int, ...]]:
     return lambda s: tuple(s.split(separator) | select(int))
 
 
-def find_valid(rules: Iterable[Rule], updates: Iterable[Update]) -> Iterable[tuple[int, ...]]:
-    return updates | where(partial(is_valid, rules=rules))
-
-
-def is_valid(update: Update, rules: Iterable[Rule]) -> bool:
-    return len(update) == 0 or (not any(rules | where(partial(contravenes, update))) and is_valid(update[1:], rules))
-
-
-def contravenes(update: Update, rule: Rule) -> bool:
-    return rule[1] == update[0] and rule[0] in update
-
-
 def middle(seq: tuple[int]) -> int:
     return seq[len(seq) // 2]
+
+
+def page_keyfunc(rules: Iterable[Rule]) -> Callable[[int], Any]:
+    def _compare(a: int, b: int) -> int:
+        for rule in rules:
+            if rule[0] == a and rule[1] == b:
+                return -1
+            if rule[0] == b and rule[1] == a:
+                return 1
+        return 0
+
+    return cmp_to_key(_compare)
 
 
 class Solution(TextSolution):
@@ -48,12 +48,11 @@ class Solution(TextSolution):
     @answer(6034)
     def part_1(self) -> int:
         rules, updates = parse_input(self.input)
-        return sum(find_valid(rules, updates) | select(middle))
+        sort_it = partial(sorted, key=page_keyfunc(rules))
+        return sum(updates | where(lambda u: tuple(sort_it(u)) == u) | select(middle))
 
-    # @answer(1234)
+    @answer(6305)
     def part_2(self) -> int:
-        pass
-
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
+        rules, updates = parse_input(self.input)
+        sort_it = partial(sorted, key=page_keyfunc(rules))
+        return sum(updates | where(lambda u: tuple(sort_it(u)) != u) | select(sort_it) | select(middle))
