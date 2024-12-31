c307a7ba2763f0d515ae8e4e5ba4e70f8e8bd55f
Blair Conrad <blair@blairconrad.com>
2024-12-31 08:24:38 -0500

    Adapt 2024 Day  9 Part 1 before solving Part 2

--- solutions/2024/day_09/solution.py
+++ solutions/2024/day_09/solution.py
@@ -3,44 +3,12 @@
 # puzzle prompt: https://adventofcode.com/2024/day/9
 
 from collections.abc import Callable, Iterable
-from operator import mul
+from dataclasses import dataclass
 from typing import Any
 
 from pipe import Pipe, select
-from pipe import enumerate as pipe_enumerate
 
 from ...base import TextSolution, answer
-
-
-def expand_blocks(blocks: Iterable[int]) -> Iterable[int | None]:
-    expanded_blocks = []
-    is_file = True
-    file_number = 0
-    for block in blocks:
-        if is_file:
-            expanded_blocks += [file_number] * block
-            file_number += 1
-            is_file = False
-        else:
-            expanded_blocks += [None] * block
-            is_file = True
-    return expanded_blocks
-
-
-def pack(expanded_blocks: list[int | None]) -> list[int | None]:
-    insert_index = 0
-
-    while insert_index < len(expanded_blocks):
-        if expanded_blocks[insert_index] is not None:
-            insert_index += 1
-            continue
-        if expanded_blocks[-1] is None:
-            expanded_blocks.pop(-1)
-            continue
-        expanded_blocks[insert_index] = expanded_blocks.pop(-1)
-        insert_index += 1
-
-    return expanded_blocks
 
 
 @Pipe
@@ -49,20 +17,81 @@ def starmap(iterable: Iterable[tuple[Any, ...]], func: Callable[[tuple[Any, ...]
         yield func(*args)
 
 
+@dataclass
+class File:
+    id: int
+    start: int
+    length: int
+
+    def weight(self) -> int:
+        return int(self.id * self.length * (self.start + (self.length - 1) / 2))
+
+
+@dataclass
+class Space:
+    start: int
+    length: int
+
+
+def parse_input(input_str: str) -> tuple[list[File], list[Space]]:
+    files = []
+    spaces = []
+
+    block_sizes = input_str | select(int)
+    is_file = True
+    file_id = 0
+    next_position = 0
+    for block_size in block_sizes:
+        if block_size > 0:
+            if is_file:
+                files.append(File(file_id, next_position, block_size))
+                file_id += 1
+            else:
+                spaces.append(Space(next_position, block_size))
+
+        next_position += block_size
+        is_file = not is_file
+
+    return (files, spaces)
+
+
+def expand_files(files: list[File]) -> list[File]:
+    expanded_files = []
+    for file in files:
+        expanded_files.extend([File(file.id, start, 1) for start in range(file.start, file.start + file.length)])
+    return expanded_files
+
+
+def pack(files: list[File], spaces: list[Space]) -> None:
+    next_file_index = len(files) - 1
+    while len(spaces) > 0 and spaces[0].start < files[next_file_index].start:
+        for i in range(len(spaces)):
+            if spaces[i].length == files[next_file_index].length:
+                files[next_file_index].start = spaces[i].start
+                spaces.pop(i)
+                break
+            if spaces[i].length > files[next_file_index].length:
+                files[next_file_index].start = spaces[i].start
+                spaces[i].start += files[next_file_index].length
+                spaces[i].length -= files[next_file_index].length
+                break
+        next_file_index -= 1
+
+
 class Solution(TextSolution):
     _year = 2024
     _day = 9
 
     @answer(6370402949053)
     def part_1(self) -> int:
-        blocks = self.input | select(int)
-        expanded_blocks = expand_blocks(blocks)
-        self.debug(expanded_blocks)
-
-        packed_blocks = pack(expanded_blocks)
-        self.debug(packed_blocks)
-
-        return sum(packed_blocks | pipe_enumerate | starmap(mul))
+        files, spaces = parse_input(self.input)
+        self.debug(files)
+        self.debug(spaces)
+        files = expand_files(files)
+        self.debug(files)
+        pack(files, spaces)
+        self.debug(files)
+        return sum(files | select(File.weight))
 
     # @answer(1234)
     def part_2(self) -> int:
