dd9bcf2c8436a3aa8dff66f3d5fbcd84b89600f7
Blair Conrad <blair@blairconrad.com>
2025-01-13 21:48:43 -0500

    Solve 2024 Day 19 Part 2

--- solutions/2024/day_19/solution.py
+++ solutions/2024/day_19/solution.py
@@ -6,8 +6,6 @@ from functools import cache
 
 from pipe import select, where
 
-from solutions.utils.iterables import how_many
-
 from ...base import StrSplitSolution, answer
 
 
@@ -16,9 +14,15 @@ class Maker:
         self.patterns = patterns
 
     @cache
-    def can_make(self, design: str) -> bool:
-        return len(design) == 0 or any(
-            design.startswith(pattern) and self.can_make(design[len(pattern) :]) for pattern in self.patterns
+    def ways_to_make(self, design: str) -> int:
+        return (
+            sum(
+                self.patterns
+                | where(design.startswith)
+                | select(lambda pattern: self.ways_to_make(design[len(pattern) :]))
+            )
+            if len(design) > 0
+            else 1
         )
 
 
@@ -26,18 +30,11 @@ class Solution(StrSplitSolution):
     _year = 2024
     _day = 19
 
-    @answer(330)
-    def part_1(self) -> int:
+    @answer((330, 950763269786650))
+    def solve(self) -> tuple[int, int]:
         patterns = self.input[0].split(", ")
         designs = list(self.input[2:] | select(str.strip))
 
         maker = Maker(patterns)
-        return how_many(designs | where(maker.can_make))
-
-    # @answer(1234)
-    def part_2(self) -> int:
-        pass
-
-    # @answer((1234, 4567))
-    # def solve(self) -> tuple[int, int]:
-    #     pass
+        number_of_ways = list(designs | select(maker.ways_to_make) | where(lambda x: x != 0))
+        return len(number_of_ways), sum(number_of_ways)
