ecd8ae23f89aead3e27fb17f6b22a6e16bf799f1
Blair Conrad <blair@blairconrad.com>
2025-12-04 20:35:20 -0500

    Adapt 2025 Day  2 Part 1 before solving Part 2

--- solutions/2025/day_02/solution.py
+++ solutions/2025/day_02/solution.py
@@ -6,14 +6,20 @@ from pipe import select, traverse, where
 
 from ...base import StrSplitSolution, answer
 
-
-def is_repeated_chunk(candidate: str) -> bool:
-    chunk_length, leftover = divmod(len(candidate), 2)
-    return leftover == 0 and candidate == 2 * candidate[:chunk_length]
+EXACTLY_TWO_CHUNKS = 2
 
 
-def is_invalid(candidate: int) -> bool:
-    return is_repeated_chunk(str(candidate))
+def is_x_repeated_chunks(x: int, candidate: str) -> bool:
+    chunk_length, leftover = divmod(len(candidate), x)
+    return leftover == 0 and candidate == candidate[:chunk_length] * x
+
+
+def repeated_chunk_count(candidate: int) -> int:
+    candidate_str = str(candidate)
+    try:
+        return next(range(2, len(candidate_str) + 1) | where(lambda x: is_x_repeated_chunks(x, candidate_str)))
+    except StopIteration:
+        return 1
 
 
 def parse_range(string_range: str) -> range:
@@ -29,7 +35,9 @@ class Solution(StrSplitSolution):
 
     @answer(38310256125)
     def part_1(self) -> int:
-        return sum(self.input | select(parse_range) | traverse | where(is_invalid))
+        return sum(
+            self.input | select(parse_range) | traverse | where(lambda s: repeated_chunk_count(s) == EXACTLY_TWO_CHUNKS)
+        )
 
     # @answer(1234)
     def part_2(self) -> int:
